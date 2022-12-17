e4883179d0b047eb80a434c09097d85fbb7b5025
stefandotl <p.sonstigemails@web.de>
2022-12-17 23:03:33 +0100

    master: refactored package name

--- src/main/java/com/example/app/UserController.java
+++ src/main/java/com/example/app/UserController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.app;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,7 +14,6 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-
 
     @GetMapping("/user")
     @ResponseBody
--- src/main/java/com/example/app/UserService.java
+++ src/main/java/com/example/app/UserService.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.app;
 
 import org.springframework.stereotype.Service;
 
